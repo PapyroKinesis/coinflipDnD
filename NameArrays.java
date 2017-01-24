1d9958cd53d4e7b30c5348f508fea0efcaf01365
PapyroKinesis <norapatrerng@gmail.com>
2017-01-24 18:53:45 -0500

    Added more arrays

--- NameArrays.java
+++ NameArrays.java
@@ -6,14 +6,28 @@ public class NameArrays {
 		"fairy", "owlbear"
 	};
 	
+	static String[][] sentences = {
+			{
+				"There is a ", "Visible is a "
+			},
+			{
+				" far away.", " in the distance."
+			}
+	}
+	
 	static Coinflip flip = new Coinflip();
 	static int numberOfNouns = nouns.length - 1;
+	static int numberOfSentences = sentences.length - 1;
 	
 	public static void main(String[] args) {
+		/*
 		System.out.println(nouns[flip.Flip(numberOfNouns)]);
 		System.out.println(nouns[flip.Flip(numberOfNouns)]);
 		System.out.println(nouns[flip.Flip(numberOfNouns)]);
 		System.out.println(nouns[flip.Flip(numberOfNouns)]);
 		System.out.println(nouns[flip.Flip(numberOfNouns)]);
+		*/
+		
+		
 	}
 }
