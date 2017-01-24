7325c40095aaf27f15673543328b9dcce72d8289
PapyroKinesis <norapatrerng@gmail.com>
2017-01-24 18:13:08 -0500

    Did some renaming
    Also removed unnecessary

--- NameArrays.java
+++ NameArrays.java
@@ -1,17 +1,19 @@
 package coinflipping_dnd;
 
 public class NameArrays<E> {
-	static String[] names = {
+	static String[] monsters = {
 		"slime", "skeleton", "spirit", "goblin", "wolf",
 		"fairy"
 	};
 	
 	static Coinflip flip = new Coinflip();
+	static int sizeOfMonsters = monsters.length - 1;
 	
 	public static void main(String[] args) {
-		
-		// System.out.println();
-		System.out.println(names[flip.Flip(5)]);
-		
+		System.out.println(monsters[flip.Flip(sizeOfMonsters)]);
+		System.out.println(monsters[flip.Flip(sizeOfMonsters)]);
+		System.out.println(monsters[flip.Flip(sizeOfMonsters)]);
+		System.out.println(monsters[flip.Flip(sizeOfMonsters)]);
+		System.out.println(monsters[flip.Flip(sizeOfMonsters)]);
 	}
 }
