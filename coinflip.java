80845aaa2b76ea3440ba756d7c2d7594b34a98ff
PapyroKinesis <norapatrerng@gmail.com>
2017-01-24 19:55:46 -0500

    No more comments

--- coinflip.java
+++ coinflip.java
@@ -1,11 +1,6 @@
 package coinflipping_dnd;
 
 public class Coinflip {
-	// static int count = 0;
-	// static int die = 1;
-	// static double flip = Math.round(Math.random()*die);
-	// static int flip = (int) Math.round(Math.random());
-	
 	int Flip(int die) {
 		int flip = (int) Math.round(Math.random()*die);
 		return flip;
