17b0388a8774b20b53d7e08eaeefacbfd0c342b8
PapyroKinesis <norapatrerng@gmail.com>
2017-01-27 20:42:03 -0500

    Added enemy attack/defending.

--- src/coinflip_dnd/BattleSim.java
+++ src/coinflip_dnd/BattleSim.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class BattleSim {
 
-    String string = Sentence.monster;
+    String name = Sentence.monster;
+    Monster victim = new Monster();
 
     public static void BattleSim() {
         System.out.println("The " + Sentence.monster + " engages in battle!\nCall heads or tails to strike!");
@@ -15,6 +16,22 @@ public class BattleSim {
         }
         else {
             System.out.println("You attacked, and hit!");
+            
         }
+        EnemyAtk();
+    }
+    public static void EnemyAtk() {
+        System.out.println("The " + Sentence.monster + " swings!\nCall heads or tails to defend!");
+        Scanner scAtk = new Scanner(System.in);
+        String input = scAtk.nextLine();
+        if(input.equals(Sentence.Attack())) {
+            System.out.println("You failed at blocking!");
+            NameArrays.hp = NameArrays.hp - 1;
+            System.out.println("You have " + NameArrays.hp + " points of health left!");
+        }
+        else {
+            System.out.println("You swiftly evaded!");
+        }
+        BattleSim();
     }
 }
