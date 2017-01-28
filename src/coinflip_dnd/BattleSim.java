6a6368f2248492bf0770820100cfbbb0d85b5daa
PapyroKinesis <norapatrerng@gmail.com>
2017-01-27 20:06:53 -0500

    Update BattleSim.java

--- src/coinflip_dnd/BattleSim.java
+++ src/coinflip_dnd/BattleSim.java
@@ -1,12 +1,16 @@
 package coinflip_dnd;
 
+import java.util.Scanner;
+
 public class BattleSim {
-    
+
     String string = Sentence.monster;
-    
+
     public static void BattleSim() {
-        System.out.println("The " + Sentence.monster + " engages in battle!");
-        if(Sentence.Attack() == true) {
+        System.out.println("The " + Sentence.monster + " engages in battle!\nCall heads or tails to strike!");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if(input.equals(Sentence.Attack())) {
             System.out.println("You missed!");
         }
         else {
