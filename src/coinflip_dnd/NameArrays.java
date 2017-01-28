609289433f6e86f110367976380f5668b03807fd
PapyroKinesis <norapatrerng@gmail.com>
2017-01-27 19:35:23 -0500

    Update NameArrays.java

--- src/coinflip_dnd/NameArrays.java
+++ src/coinflip_dnd/NameArrays.java
@@ -1,34 +1,36 @@
 package coinflip_dnd;
 
 public class NameArrays {
-	public static String[] nouns = {
-		"slime", "skeleton", "spirit", "goblin", "wolf",
-		"fairy", "group of owlbears"
-	};
-	
-	public static String[][] sentences = {
-			{	//proximityFrag
-				"Off in the distance ", "Around the corner ", "Nearby "
-			},
-			{	//verbFrag
-				"rests a ", "is a ", "you see a "
-			},
-			{	//prepFrag
-				" in the road.", " in a small copse of trees.", " on the ground."
-			},
-			{	//punct
-				".", "!"
-
-			}
-	};
-	
-	static Coinflip flip = new Coinflip();
-	static Sentence generate = new Sentence();
-	static int numberOfNouns = nouns.length - 1;
-	public static int numberOfSentences = sentences.length - 1;
-	
-	public static void main(String[] args) {
-		generate.Sentence();
-
-	}
+    public static String[] nouns = {
+        "slime", "skeleton", "spirit", "goblin", "wolf",
+        "fairy", "group of owlbears"
+    };
+    
+    public static String[][] sentences = {
+            {   //proximityFrag
+                "Off in the distance ", "Around the corner ", "Nearby "
+            },
+            {   //verbFrag
+                "rests a ", "is a ", "you see a "
+            },
+            {   //prepFrag
+                " in the road.", " in a small copse of trees.", " on the ground."
+            },
+            {   //punct
+                ".", "!"
+            }
+    };
+    
+    static Coinflip flip = new Coinflip();
+    static Sentence generate = new Sentence();
+    static int numberOfNouns = nouns.length - 1;
+    public static int numberOfSentences = sentences.length - 1;
+    
+    
+    // Run the program!
+    public static void main(String[] args) {
+        generate.Sentence();
+        // System.out.println(Sentence.monster);
+        BattleSim.BattleSim();
+    }
 }
