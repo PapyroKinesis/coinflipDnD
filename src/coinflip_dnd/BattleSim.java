4e1725c8e3cad727b493abb53fd8655770dbc36b
PapyroKinesis <norapatrerng@gmail.com>
2017-02-01 20:46:58 -0500

    Update BattleSim.java

--- src/coinflip_dnd/BattleSim.java
+++ src/coinflip_dnd/BattleSim.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class BattleSim {
 
     String name = Sentence.monster;
-    Monster victim = new Monster();
-
+    static Monster victim = new Monster();
+    static int start = 0;
+    
     public static void BattleSim() {
-        System.out.println("The " + Sentence.monster + " engages in battle!\nCall heads or tails to strike!");
+        if(start == 0) {
+            System.out.println("The " + Sentence.monster + " engages in battle!");
+        }
+        System.out.println("Call heads or tails to strike!");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         if(input.equals(Sentence.Attack())) {
             System.out.println("You missed!");
+            EnemyAtk();
         }
         else {
             System.out.println("You attacked, and hit!");
-            
+            victim.hp = victim.hp - NameArrays.atk;
+            System.out.println("The " + victim.name + " is at " + victim.hp + " hitpoints!");
+            if(victim.hp > 0) {
+                EnemyAtk();
+            }
+            else {
+                System.out.println("The " + victim.name + " has died!");
+                System.out.println("");
+                NameArrays.Reset();
+            }
         }
-        EnemyAtk();
+        
     }
     public static void EnemyAtk() {
         System.out.println("The " + Sentence.monster + " swings!\nCall heads or tails to defend!");
