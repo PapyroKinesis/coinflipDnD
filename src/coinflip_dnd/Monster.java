a6d2c2b053f7c864dab9570b5c97cddcdadeab2a
PapyroKinesis <norapatrerng@gmail.com>
2017-02-02 00:04:15 -0500

    Update Monster.java

--- src/coinflip_dnd/Monster.java
+++ src/coinflip_dnd/Monster.java
@@ -1,16 +1,36 @@
 package coinflip_dnd;
 
-class Monster {
-    // Monster statistics
-    int hp = 1;
-    int atkMonster = 1;
-    String name;
-    
+public class Monster {
+	// Monster statistics
+	static int hpM = 1;
+	static int atkMonster = 1;
+	static int lvl = 1;
+	static String name = Sentence.monster;
+	static int[][] levels = {
+			{
+				1, 2
+			},
+			{
+				1, 2, 3
+			}
+	};
+
+	public void setLvl() {
+		lvl = Coinflip.Flip(NameArrays.mLvl);
+		if (lvl < 1) {
+			lvl = 1;
+		}
+	}
+	public void setHp() {
+		this.hpM = levels[this.lvl][Coinflip.Flip(levels[lvl].length - 1)];
+	}
+	public static void setUp() {
+		hpM = 1;
+		atkMonster = 1;
+		lvl = 1;
+		name = Sentence.monster;
+	}
     public Monster() {
-
-    }
-    public void setHp(int range) {
-        Coinflip.Flip(range);
-    }
-
+	}
 }
+    
