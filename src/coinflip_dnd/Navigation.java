2ddcdad17eedf5abec9252c08e0ae76f2a137e29
PapyroKinesis <norapatrerng@gmail.com>
2017-02-07 23:16:59 -0500

    Made it work...

--- src/coinflip_dnd/Navigation.java
+++ src/coinflip_dnd/Navigation.java
@@ -1,79 +1,153 @@
 package coinflip_dnd;
 
-import java.lang.reflect.Array;
+import java.util.Scanner;
+import com.sun.xml.internal.bind.v2.runtime.Name;
 
 public class Navigate {
 
-    static int[] rooms = {
-            0, 1, 0,
-            1, 1, 1,
-            0, 0, 1
-    };
+	static int[] rooms = {
+			0, 1, 0,
+			1, 1, 1,
+			0, 0, 1
+	};
+	static String[] encounters = {
+			"chest", "monster", "monsters", "nothing", "nothing", "nothing", "nothing",
+	};
+	static String[] chestContents = {
+			" a potion", " a surprise", "nothing"
+	};
+	static String contents;
+	static String name = encounters[Coinflip.Flip(encounters.length - 1)];
 
-    static int inRoom = 4;
-    static Array[] paths = new Array[4];
-    static String north = null;
-    static String south = null;
-    static String east = null;
-    static String west = null;
+	static int inRoom = 4;
+	//static Array[] paths = new Array[4];
+	static String north = null;
+	static String south = null;
+	static String east = null;
+	static String west = null;
 
-    public static void checkDir() {
-        if(inRoom > 2 && inRoom < 6) {
-            north = checkNorth();
-            south = checkSouth();
-        }
+	public static void checkDir() {
+		Nullify();
+		north = checkNorth();
+		south = checkSouth();
+		west = checkWest();
+		east = checkEast();
+		if(north != null && south != null) {
+			System.out.println("There is a path:");
+			if(north.equals("north")) {
+				System.out.println("north");
+			}
+			if(south.equals("south")) {
+				System.out.println("south");
+			}
+			if(east.equals("east")) {
+				System.out.println("east");
+			}
+			if(west.equals("west")) {
+				System.out.println("west");
+			}
+		}
+		name = encounters[Coinflip.Flip(encounters.length - 1)];
+		CheckEncounter();
+		Travel();
+	}
+	public static void Travel() {
+		System.out.println("Choose a direction to travel:");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		if(input.equals(north) && north.equals("north")) {
+			inRoom = inRoom - 3;
+			NameArrays.worldgen.EnvironmentDescription();
+		}
+		if(input.equals(south) && south.equals("south")) {
+			inRoom = inRoom + 3;
+			NameArrays.worldgen.EnvironmentDescription();
+		}
+		if(input.equals(west) && west.equals("west")) {
+			inRoom = inRoom - 1;
+			NameArrays.worldgen.EnvironmentDescription();
+		}
+		if(input.equals(east) && east.equals("east")) {
+			inRoom = inRoom + 1;
+			NameArrays.worldgen.EnvironmentDescription();
+		}
+		else {
+			//System.out.println("Invalid input.");
+			checkDir();
+			Travel();
+		}
+	}
+	public static void Nullify() {
+		north = null;
+		south = null;
+		east = null;
+		west = null;
+	}
+	public static String checkNorth() {
+		if((inRoom - 3 ) >= 0 && rooms[inRoom - 3] > 0) {
+			return "north";
+		}
+		else {
+			return "no way north";
+		}
+	}
+	public static String checkSouth() {
+		if((inRoom + 3) <= 8 && rooms[inRoom + 3] > 0) {
+			return "south";
+		}
+		else {
+			return "no way south";
+		}
+	}
+	public static String checkWest() {
+		if(inRoom != 0 && inRoom != 3 && inRoom != 6 
+				&& (inRoom - 1) <= 8 && rooms[inRoom - 1] > 0) {
+			return "west";
+		}
+		else {
+			return "no way west";
+		}
+	}
+	public static String checkEast() {
+		if((inRoom + 1) <= 8 && rooms[inRoom + 1] > 0 &&
+				inRoom != 2 && inRoom != 5 && inRoom != 8) {
+			return "east";
+		}
+		else {
+			return "no way east";
+		}
+	}
 
-    }
+	public static void Navigate() {
+		checkDir();
+		Travel();
+	}
 
-    public static String checkNorth() {
-        if((inRoom - 3 ) > 0 && rooms[inRoom - 3] > 0) {
-            return "north";
-        }
-        else {
-            return "no way north";
-        }
-    }
-    public static String checkSouth() {
-        if((inRoom + 3) < 8 && rooms[inRoom + 3] > 0) {
-            return "south";
-        }
-        else {
-            return "no way south";
-        }
-    }
-    public static String checkWest() {
-        if(rooms[inRoom - 1] >= 0 && rooms[inRoom - 1] <= 8) {
-            return "west";
-        }
-        else {
-            return "no way west";
-        }
-    }
-    public static String checkEast() {
-        if(rooms[inRoom + 1] >= 0 && rooms[inRoom + 1] <= 8) {
-            return "east";
-        }
-        else {
-            return "no way east";
-        }
-    }
-
-    public static void Navigate() {
-        checkDir();
-        if(north != null && south != null) { //&& west != null && east != null) {
-            System.out.println("There is a path:");
-            if(north.equals("north")) {
-                System.out.println("north");
-            }
-            if(south.equals("south")) {
-                System.out.println("south");
-            }
-            if(east.equals("east")) {
-                System.out.println("east");
-            }
-            if(west.equals("west")) {
-                System.out.println("west");
-            }
-        }
-    }
+	public static void CheckEncounter() {
+		if(name.equals("chest")) {
+			System.out.println("You stumble upon a chest! Open it?");
+			System.out.println("Type 'open' or 'leave'.");
+			Scanner scan = new Scanner(System.in);
+			String input = scan.nextLine();
+			if(input.equals("open")) {
+				contents = chestContents[Coinflip.Flip(chestContents.length) - 1];
+				System.out.println("Within the chest was" + contents + 
+						".");
+				
+				checkDir();
+			}
+			if(input.equals("leave")) {
+				System.out.println("Best not to risk it.");
+			}
+		}
+		if(name.equals("monster")) {
+			
+			BattleSim.BattleSim();
+		}
+		if(name.equals("nothing")) {
+			System.out.println("There's nothing.");
+		}
+		//checkDir();
+		//Travel();
+	}
 }
